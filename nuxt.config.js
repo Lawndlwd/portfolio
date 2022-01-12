bf154c63c0d33890dbf2a17be999e58bd1758be1
Levende <levende.mohammad@gravity.earth>
2022-01-12 16:27:11 +0100

    first commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   colorMode: {
     classSuffix: "",
   },
-
+  buildDir: "dist",
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "Levende's Portfolio",
