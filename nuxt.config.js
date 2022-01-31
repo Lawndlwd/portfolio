da6c21dd8f8c10cf4143482190f4bf1d8809f874
Levende <lwdlwd95@gmail.com>
2022-01-31 14:11:09 +0100

    add mete data

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,15 +8,64 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "Levende Mohammad",
+    title: "Levende Mohammad— Fullstack JavaScript Engineer",
     htmlAttrs: {
       lang: "en",
     },
+
     meta: [
       { charset: "utf-8" },
+      {
+        name: "title",
+        content: "Levende Mohammad— Fullstack JavaScript Engineer ",
+      },
+      {
+        name: "description",
+        content:
+          "Self-taught JavaScript developer ,living near Paris, specialized in React and Vue Focused on developing accessible, and responsive web applications, as well as improving my skills within the JavaScript ecosystem, working with current web technologies such as Vue,Typescript, React, Nuxt.js & Node.js to create data-driven, feature-rich applications.",
+      },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" },
       { name: "format-detection", content: "telephone=no" },
+      { property: "og:type", content: "website" },
+      {
+        property: "og:title",
+        content: "Levende Mohammad— Fullstack JavaScript Engineer ",
+      },
+      {
+        property: "og:url",
+        content: "https://levende.net ",
+      },
+
+      {
+        property: "og:description",
+        content:
+          "Self-taught JavaScript developer ,living near Paris, specialized in React and Vue Focused on developing accessible, and responsive web applications, as well as improving my skills within the JavaScript ecosystem, working with current web technologies such as Vue,Typescript, React, Nuxt.js & Node.js to create data-driven, feature-rich applications.",
+      },
+      {
+        property: "og:image",
+        content:
+          "https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png",
+      },
+      {
+        property: "twitter:title",
+        content: "Levende Mohammad— Fullstack JavaScript Engineer ",
+      },
+      {
+        property: "twitter:description",
+        content:
+          "Self-taught JavaScript developer ,living near Paris, specialized in React and Vue Focused on developing accessible, and responsive web applications, as well as improving my skills within the JavaScript ecosystem, working with current web technologies such as Vue,Typescript, React, Nuxt.js & Node.js to create data-driven, feature-rich applications.",
+      },
+      { property: "twitter:card", content: "summary_large_image" },
+      {
+        property: "twitter:image",
+        content:
+          "https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png",
+      },
+      {
+        property: "twitter:url",
+        content: "https://levende.net ",
+      },
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
